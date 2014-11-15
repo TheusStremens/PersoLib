116acc057a9eaf0c2d075fa9683e1ef1ccb112d6
Philippe Correia <philippelbc@dcc.ufba.br>
2014-11-15 16:21:32 -0300

    Atualização BUsuario
    Novas validações na classe BUsuario

--- VSProject/PersoLib_DAL/Business/Usuario.cs
+++ VSProject/PersoLib_DAL/Business/Usuario.cs
@@ -11,11 +11,20 @@ namespace PersoLib_DAL
                 lsMensagemOperacao = string.Empty;
                 bool lbValidado = true;
 
-                if (String.Compare(aoUsuario.USR_senha, aoUsuario.USR_repete_senha, false) != 0)
+                // Confere comprimento da senha
+                if (aoUsuario.USR_senha.Length < 4 || aoUsuario.USR_senha.Length >= 12)
                 {
-                    lsMensagemOperacao = "O campo repete senha deve ser igual ao campo senha!";
+                    lsMensagemOperacao = "A senha deve conter de 4 a 12 caracteres!";
                     lbValidado = false;
                 }
+
+                // Confere se os campos 'senha' e 'repete senha' são iguais
+                if (String.Compare(aoUsuario.USR_senha, aoUsuario.USR_repete_senha) != 0)
+                {
+                    lsMensagemOperacao = "O campo 'repete senha' deve ser igual ao 'campo senha'!";
+                    lbValidado = false;
+                }
+
 
                 if (lbValidado)
                 {
