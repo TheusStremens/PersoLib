16dc41275e4e2bdda85752a0fa71c65e4ca77776
fredryco <bskfred@gmail.com>
2014-11-16 13:53:15 -0300

    Atualização BUsuario
    Criação de novos metodos de verificação e validação

--- VSProject/PersoLib_DAL/Business/Usuario.cs
+++ VSProject/PersoLib_DAL/Business/Usuario.cs
@@ -1,30 +1,56 @@
 ﻿using System;
 
+
 namespace PersoLib_DAL
 {
-    public static class Business
+    public static partial class Business
     {
         public class Usuario
         {
-            public bool InserirNovoUsuario(Entity.Usuario aoUsuario, out string lsMensagemOperacao)
+            internal bool VerificaExistenciaEmail(Entity.Usuario aoUsuario, out string lsMensagemOperacao)
+            {
+                lsMensagemOperacao = string.Empty;
+                if (new DAL.Usuario().ExisteUsuarioEmail(aoUsuario) != 1)
+                {
+                    lsMensagemOperacao = "Email esta errado ou não existe cadastro!";
+                    return false;
+                }
+                return true;
+            }
+
+            internal bool InserirNovoUsuario(Entity.Usuario aoUsuario, out string lsMensagemOperacao)
             {
                 lsMensagemOperacao = string.Empty;
                 bool lbValidado = true;
 
-                // Confere comprimento da senha
-                if (aoUsuario.USR_senha.Length < 4 || aoUsuario.USR_senha.Length >= 12)
-                {
-                    lsMensagemOperacao = "A senha deve conter de 4 a 12 caracteres!";
+                //Confere se o nome da pessoa é muito curto
+                if (!(Util.VerificarNome(aoUsuario.USR_nome,out lsMensagemOperacao)))
+              {
                     lbValidado = false;
-                }
+              }
+
+                // Confere se email é valido
+                if (!(Util.VerificaValidadeEmail(aoUsuario.USR_email, out lsMensagemOperacao)))
+                {
+                  lbValidado = false;
+              }
+                //Confere se o email informa ja foi utilizado
+                if (!(VerificaExistenciaEmail(aoUsuario, out lsMensagemOperacao)))
+              {
+                  lbValidado = false;
+              }
+
+                //Confere se tamanho da senha é valido
+                if (!(Util.VerificaTamanhoSenha(aoUsuario.USR_senha, out lsMensagemOperacao)))
+              {
+                  lbValidado = false;
+              }
 
                 // Confere se os campos 'senha' e 'repete senha' são iguais
-                if (String.Compare(aoUsuario.USR_senha, aoUsuario.USR_repete_senha) != 0)
-                {
-                    lsMensagemOperacao = "O campo 'repete senha' deve ser igual ao 'campo senha'!";
-                    lbValidado = false;
-                }
-
+                if (!(Util.VerificaIgualdadeSenha(aoUsuario.USR_senha, aoUsuario.USR_repete_senha, out lsMensagemOperacao)))
+              {
+                  lbValidado = false;
+              }
 
                 if (lbValidado)
                 {
@@ -37,6 +63,74 @@ namespace PersoLib_DAL
 
                 return lbValidado;
             }
+
+            
+            internal int VerificarLogin(Entity.Usuario aoUsuario, out string lsMensagemOperacao)
+            {
+                lsMensagemOperacao = string.Empty;
+
+                if (!(VerificaExistenciaEmail(aoUsuario, out lsMensagemOperacao)))
+                {
+                    return -1;
+                }
+                else if (new DAL.Usuario().LoginUsuario(aoUsuario) == -1)
+                    {
+                        lsMensagemOperacao = "Senha invalida para este login!";
+                        return -1;
+                    }
+                
+                return new DAL.Usuario().LoginUsuario(aoUsuario);
+            }
+
+            internal bool AlterarUsuaio(Entity.Usuario aoUsuario, out string lsMensagemOperacao)
+            {
+                lsMensagemOperacao = string.Empty;
+                bool lbValidado = true;
+
+                //Confere se o nome da pessoa é muito curto
+                if (!(Util.VerificarNome(aoUsuario.USR_nome, out lsMensagemOperacao)))
+                {
+                    lbValidado = false;
+                }
+
+                // Confere se email é valido
+                if (!(Util.VerificaValidadeEmail(aoUsuario.USR_email, out lsMensagemOperacao)))
+                {
+                    lbValidado = false;
+                }
+                //Confere se o email informa ja foi utilizado
+                if (!(VerificaExistenciaEmail(aoUsuario, out lsMensagemOperacao)))
+                {
+                    lbValidado = false;
+                }
+
+                //Confere se tamanho da senha é valido
+                if (!(Util.VerificaTamanhoSenha(aoUsuario.USR_senha, out lsMensagemOperacao)))
+                {
+                    lbValidado = false;
+                }
+
+                // Confere se os campos 'senha' e 'repete senha' são iguais
+                if (!(Util.VerificaIgualdadeSenha(aoUsuario.USR_senha, aoUsuario.USR_repete_senha, out lsMensagemOperacao)))
+                {
+                    lbValidado = false;
+                }
+
+                if(string.IsNullOrEmpty(aoUsuario.USR_nome) || string.IsNullOrEmpty(aoUsuario.USR_email){
+                    lsMensagemOperacao = "Algum campo não foi preenchido. Favor Verificar dados!";
+                }
+
+                if (lbValidado)
+                {
+                    if(new DAL.Usuario().AtualizarDadosUsuario(aoUsuario) == -1){
+                        lsMensagemOperacao = "Ocorreu algum erro no servidor!";
+                        lbValidado = false;
+                    }
+                }
+
+                return lbValidado;
+            }
+
         }
     }
 }
---
+++ VSProject/PersoLib_DAL/Util.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersoLib_DAL
+{
+    public static class Util
+    {
+
+        public static bool VerificaValidadeEmail(string strEmail, out string lsMensagemOperacao)
+        {
+            lsMensagemOperacao = string.Empty;
+            string strModelo = "^([0-9a-zA-Z]([-.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$";
+            if (!(System.Text.RegularExpressions.Regex.IsMatch(strEmail, strModelo)))
+            {
+                lsMensagemOperacao = "Formato de email incorreto!";
+                return false;
+            }
+            return true;
+        }
+
+        public static bool VerificaTamanhoSenha(string str_Senha, out string lsMensagemOperacao)
+        {
+             lsMensagemOperacao = string.Empty;
+            if (str_Senha.Length < 4 || str_Senha.Length >= 12)
+            {
+                lsMensagemOperacao = "A senha deve conter de 4 a 12 caracteres!";
+                return false;
+            }
+            return true;
+        }
+
+        public static bool VerificaIgualdadeSenha(string str_Senha, string str_Repete_Senha, out string lsMensagemOperacao)
+        {
+            lsMensagemOperacao = string.Empty;
+            if (String.Compare(str_Senha, str_Repete_Senha, false) != 0)
+            {
+                lsMensagemOperacao = "O campo 'repete senha' deve ser igual ao 'campo senha'!";
+                return false;
+            }
+            return true;
+        }
+
+        public static bool VerificarNome(string str_Nome, out string lsMensagemOperacao)
+        {
+            lsMensagemOperacao = string.Empty;
+            if (str_Nome.Length < 3)
+            {
+                lsMensagemOperacao = "Nome de tamanho invalido!";
+                return false;
+            }
+            return true;
+        }
+
+       
+
+    }
+}
