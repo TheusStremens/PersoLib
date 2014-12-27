d291500848486071874fa7d3b9a677fd472d1a66
Philippe Correia <philippelbc@dcc.ufba.br>
2014-12-27 11:39:14 -0300

    Revert "Reversão de conflito"
    This reverts commit 6e973a6a0674ba70373eaab07afc7b822a3d82be.

--- VSProject/PersoLib_DAL/DAL/Usuario.cs
+++ VSProject/PersoLib_DAL/DAL/Usuario.cs
@@ -271,8 +271,11 @@ namespace PersoLib_DAL
                 {
                     conn.Open();
 
-                    string lsSQLQuery = "update usr_usuario set USR_ativo = 1 where USR_id = @id";
-                    MySqlCommand cmd = new MySqlCommand(lsSQLQuery, conn);
+                    StringBuilder loStb = new StringBuilder();
+                    loStb.Append("UPDATE usr_usuario SET ");
+                    loStb.Append("USR_ativo = 1 ");
+                    loStb.Append(" where USR_id = @id ");
+                    MySqlCommand cmd = new MySqlCommand(loStb.ToString(), conn);
                     cmd.Parameters.AddWithValue("@id", aoUsuario.USR_id);
 
                     liResult = cmd.ExecuteNonQuery();
